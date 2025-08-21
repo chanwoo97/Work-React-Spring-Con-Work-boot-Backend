062527388c0295fca47b47d5d3eee349609296e1
chanwoo97 <jcw6223@naver.com>
2025-08-21 12:50:06 +0900

    React-Spring 연결작업 수업1

---
+++ src/main/java/repository/APlUserRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import com.busanit501.api_rest_test_jwt_react.domain.APIUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//작업 순서8
+@Repository
+public interface APlUserRepository extends JpaRepository<APIUser, String> {
+}
