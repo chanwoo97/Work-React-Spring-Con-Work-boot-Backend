cec80a2e5ccd45ff3b9aabf292383daa8e369c9e
chanwoo97 <jcw6223@naver.com>
2025-08-25 15:23:39 +0900

    React-Spring 연결작업 수업2,3

--- src/main/java/com/busanit501/api_rest_test_jwt_react/controller/SampleController.java
+++ src/main/java/com/busanit501/api_rest_test_jwt_react/controller/SampleController.java
@@ -1,6 +1,7 @@
 package com.busanit501.api_rest_test_jwt_react.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,13 @@ public class SampleController {
             description = "AAA, BBB, CCC 출력 테스트")
     @GetMapping("/doA")
 //    @PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     public List<String> doA() {
         return Arrays.asList("AAA", "BBB", "C");
     }
 
     @GetMapping("/doB")
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<String> doB() {
         return Arrays.asList("AdminAAA", "AdminBBB", "AdminCCC");
     }
